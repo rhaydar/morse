e162c8b1a8e85b541a6b777300fe6f44699b0f75
Rashedul Haydar <rh2712@columbia.edu>
2016-08-11 17:13:47 -0400

    Created morse.cpp, can produce alert sound

---
+++ morse.cpp
@@ -0,0 +1,14 @@
+// morse.cpp
+// author: rashedul haydar
+// date: 2016-08-11
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "\a" << endl;
+
+	return 0;
+}
